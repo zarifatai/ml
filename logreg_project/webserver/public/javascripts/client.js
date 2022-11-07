e80148a3047f0c85109f57fdfd02ec79ee6e2907
Zarif Atai <z.atai@protonmail.com>
2022-11-07 22:27:51 +0100

    Added functionality to send feature values to server

--- logreg_project/webserver/public/javascripts/client.js
+++ logreg_project/webserver/public/javascripts/client.js
@@ -1,7 +1,32 @@
 const server_url = 'http://localhost:3000'
-const path = '/features'
-const url = server_url + path
+// const path = '/features'
+// const url = server_url + path
 
-fetch(url)
-	.then(data => {return data.json()})
-	.then(res => {console.log(res)})
+// fetch(url)
+// 	.then(data => {return data.json()})
+// 	.then(res => {console.log(res)})
+
+function postFeatureValues() {
+	let inputs = document.getElementsByTagName('input');
+	let values = [];
+	Array.from(inputs).forEach(function (element) {
+		values.push(element.value)
+      });
+	values = values.map(Number);
+
+	const predict_path = '/predict';
+	fetch(server_url + predict_path, {
+		method: 'POST',
+		headers: {
+		  'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(values),
+	  })
+		.then((response) => response.json())
+		.then((data) => {
+		  console.log('Success:', data);
+		})
+		.catch((error) => {
+		  console.error('Error:', error);
+		});
+}
--- logreg_project/webserver/routes/index.js
+++ logreg_project/webserver/routes/index.js
@@ -1,21 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const model_api_url = 'http://localhost:8080'
+
+const url_features = model_api_url + '/features'
+features = null
+fetch(url_features)
+  .then(data => {return data.json()})
+  .then(output => {features = output});
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
+  console.log(features)
   res.render('index', {
     title: 'Skin Cancer Predictor',
+    features: features,
   });
 });
 
-router.get('/features', (req, res, next) => {
-  const model_api_url = 'http://localhost:8080'
-  const path = '/features'
-  const url = model_api_url + path
-
-  fetch(url)
+// TODO:
+// - send dictionary to model api rather than array of floats
+// - api call should be directly from client to model api rather than through nodejs server
+router.post('/predict', (req, res, next) => {
+  values = req.body
+  url_predict = model_api_url + '/predict'
+  prediction = null
+  fetch(url_predict)
     .then(data => {return data.json()})
-    .then(features => {res.send(features)});
+    .then(output => {prediction = output})
+  res.send({prediction: prediction})
 });
 
 module.exports = router;
+
